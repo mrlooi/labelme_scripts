03f0570466d960faa4aeb38f830e44aeb9d7f8bd
vincent <user.name>
2019-07-08 14:31:42 +0800

    upgraded labelme_to_coco

--- labelme_to_coco.py
+++ labelme_to_coco.py
@@ -6,109 +6,60 @@ import xml.etree.ElementTree as ET
 import copy
 import datetime
 
-def convert_datetime_to_string(dt=datetime.datetime.now(), formt="%Y-%m-%d %H:%M:%S"):
-	return dt.strftime(formt)
+from collections import defaultdict 
 
-class CocoAnnotationClass(object):
-	def __init__(self, classes, supercategory=""):
-		self.classes = classes
-		self.map_classes_idx = {c: ix+1 for ix,c in enumerate(classes)}  # coco is 1-indexed
-		self.map_idx_classes = {v:k for k,v in self.map_classes_idx.items()}
-		self.data = self._get_default_data()
-		for c,idx in self.map_classes_idx.items():
-			self._add_category(c,idx,supercategory)
+from coco_annotation import CocoAnnotationClass
 
-	def _get_default_data(self):
-		default_d = {
-			"info": {
-				"year" : 2018, 
-				"version" : "", 
-				"description" : "", 
-				"contributor" : "", 
-				"url" : "", 
-				"date_created" : convert_datetime_to_string()
-			},
-			"images": [],
-			"annotations": [],
-			"categories": [],
-			"licenses": [
-				{
-					"id" : 1, 
-					"name" : "", 
-					"url" : ""
-				}
-			]
-		}
-		return default_d
+def recursively_get_all_subchild_ids(e_id, element_ids_dict):
+	if e_id not in element_ids_dict:
+		return []
 
-	def set_classes(self, classes):
-		self.classes = classes
+	e = element_ids_dict[e_id]
 
-	def clear(self):
-		self.data = self._get_default_data()
+	parts = e.find("parts")
+	# CHECK CHILD
+	child_parts = parts.find("hasparts").text
+	all_child_ids = []
+	if child_parts and len(child_parts) > 0:
+		child_ids = [cid for cid in child_parts.split(",") if cid != e_id]
+		all_child_ids += child_ids
+		for cid in child_ids:
+			all_child_ids += recursively_get_all_subchild_ids(cid, element_ids_dict)
 
-	def _add_category(self, name, id=None, supercategory=""):
-		cat_id = len(self.data["categories"]) + 1 if id is None else id
-		cat_data = {
-					"id" : cat_id, 
-					"name" : name, 
-					"supercategory" : supercategory
-				}
-		self.data["categories"].append(cat_data)
+	return all_child_ids
 
-	def add_annot(self, id, img_id, img_cls, seg_data, is_crowd=0):
-		"""ONLY SUPPORTS seg polygons of len 1 i.e. cannot support multiple polygons that refer to the same id"""
-		if img_cls not in self.map_classes_idx:
-			print("%s not in coco classes!"%(img_cls))
-			return 
-		cat_id = self.map_classes_idx[img_cls]
-		seg_data_arr = np.array(seg_data)
-		assert(len(seg_data_arr.shape) == 2)
-		bbox = np.array([np.amin(seg_data_arr, axis=0), np.amax(seg_data_arr, axis=0)]).reshape(4)
-		bbox[2:] -= bbox[:2]
-		bbox = bbox.tolist()
-		area = cv2.contourArea(seg_data_arr)
-		annot_data =	{
-					"id" : id,
-					"image_id" : img_id,
-					"category_id" : cat_id,
-					"segmentation" : [seg_data_arr.flatten().tolist()],
-					"area" : area,
-					"bbox" : bbox,
-					"iscrowd" : is_crowd
-				}
-		self.data["annotations"].append(annot_data)
+def find_top_parent_id(e_id, element_ids_dict):
+	if e_id not in element_ids_dict:
+		return None
+	e = element_ids_dict[e_id]
 
-	def add_image(self, id, width, height, file_name, date_captured=convert_datetime_to_string()):
-		img_data =	{
-					"id" : id,
-					"width" : width,
-					"height" : height,
-					"file_name" : file_name,
-					"license" : 1,
-					"flickr_url" : "",
-					"coco_url" : "",
-					"date_captured" : date_captured
-				}
-		self.data["images"].append(img_data)
-
-	def get_annot_json(self):
-		return copy.deepcopy(self.data)
-
+	parts = e.find("parts")
+	# CHECK PARENT
+	parent = parts.find("ispartof").text
+	if parent and len(parent) > 0 and parent != e_id:
+		upper_parent = find_top_parent_id(parent, element_ids_dict)
+		if upper_parent and len(upper_parent) > 0 and upper_parent not in [parent, e_id]:
+			parent = upper_parent
+	else:
+		parent = e_id
+	return parent
 
 if __name__ == '__main__':
 	import glob
+	from natsort import natsorted
 
 	RED = (0,0,255)
 	GREEN = (0,255,0)
 
-	labelme_root = "/home/vincent/LabelMe"
+	VIS = True
+
+	labelme_root = "/home/bot/LabelMe"
 	# labelme_root = "/home/drml/hd/datasets/LabelMe/"
-	image_set = 'sm_data'
+	image_set = 'pen_dataset'
 	# image_set = 'barcodes'
 	coco_out_file = "%s.json"%(image_set)
-	SUPERCATEGORY = 'loading' 
-	CLASSES = ['box']#,'bar'] # ["barcode"]#,"box","envelope"]
+	SUPERCATEGORY = 'pen_dataset' #'unloading' 
+	CLASSES = ['pen', 'pencil'] #['box'] # ["barcode"]#,"box","envelope"]
 
 	annot_dir = osp.join(labelme_root, "Annotations", image_set)
 	img_dir = osp.join(labelme_root, "Images", image_set)
@@ -116,7 +67,8 @@ if __name__ == '__main__':
 
 	coco_annot = CocoAnnotationClass(CLASSES, SUPERCATEGORY)
 
-	annot_dir_glob = glob.glob(annot_dir + "/*.xml")
+	annot_dir_glob = natsorted(glob.glob(annot_dir + "/*.xml"))[17:18]
+
 	annot_dir_len = len(annot_dir_glob)
 	total_cnt = 0
 	for ix, annot_file in enumerate(annot_dir_glob):
@@ -137,39 +89,63 @@ if __name__ == '__main__':
 		print("Element height %d, width %d"%(element_height, element_width))
 
 		element_objects = element.findall('object')
-		e_pts_all = []
-		e_bboxes = []
+		
+		parent_child_ids = defaultdict(list)
+		element_ids = {}
+
 		cnt = 0
 		for e in element_objects:
+			# print(e.find('name').text)
 			if int(e.find('deleted').text) == 1:
 				continue
-			# print(e.find('name').text)
-			e_poly = e.find('polygon')
-			e_cls = e.find('name').text
-			e_pts = [( float(p.find('x').text), float(p.find('y').text) ) for p in e_poly.findall('pt')]
-			e_pts = np.array(e_pts).astype(np.int32)
-			e_bbox = np.array([np.amin(e_pts, axis=0), np.amax(e_pts, axis=0)]).reshape(4)
-			e_bbox[2:] -= e_bbox[:2]
-			e_pts_area = cv2.contourArea(e_pts)
-			# print(e_cls)
-			if e_cls not in CLASSES:
+			e_id = e.find("id").text
+			element_ids[e_id] = e
+
+		used_ids = []
+		for e_id in element_ids:
+
+			parent_id = find_top_parent_id(e_id, element_ids)
+			child_ids = recursively_get_all_subchild_ids(parent_id, element_ids)
+			all_ids = list(set(child_ids + [parent_id]))
+
+			valid_ids = [id for id in all_ids if id not in used_ids and id in element_ids]
+			if len(valid_ids) == 0:
 				continue
-			# print("%s (%d), %.3f"%(e_cls, category_id, e_pts_area))
-			e_bboxes.append(e_bbox)
-			e_pts_all.append(e_pts)
+			# print(all_ids)
+			used_ids += valid_ids
+
+			polygons = []
+			for id in valid_ids:
+				e = element_ids[id]
+				e_cls = e.find('name').text
+				if e_cls not in CLASSES:
+					continue
+				
+				e_poly = e.find('polygon')
+				e_pts = [( float(p.find('x').text), float(p.find('y').text) ) for p in e_poly.findall('pt')]
+				e_pts = np.array(e_pts).astype(np.int32)
+				polygons.append(e_pts)
+
+			if len(polygons) == 0:
+				continue
 
 			cnt += 1
 			total_cnt += 1
 
-			coco_annot.add_annot(total_cnt, IMG_ID, e_cls, e_pts)
+			coco_annot.add_annot(total_cnt, IMG_ID, e_cls, polygons)
 
-			# img = cv2.imread(input_img_file)
-			# img = cv2.rectangle(img, tuple(e_bbox[:2]), tuple(e_bbox[2:] + e_bbox[:2]), RED)
-			# img = cv2.putText(img, e_cls, tuple(e_bbox[:2]), cv2.FONT_HERSHEY_COMPLEX, 0.6, GREEN)
-			# img = cv2.fillPoly(img, [e_pts], GREEN)
+			if VIS:
+				img = cv2.imread(input_img_file)
+				for poly in polygons:
+					# img = cv2.rectangle(img, tuple(e_bbox[:2]), tuple(e_bbox[2:] + e_bbox[:2]), RED)
+					# img = cv2.putText(img, e_cls, tuple(e_bbox[:2]), cv2.FONT_HERSHEY_COMPLEX, 0.6, GREEN)
+					img = cv2.fillPoly(img, [poly], GREEN)
 
-			# cv2.imshow("img", img)
-			# cv2.waitKey(0)
+				ih, iw = img.shape[:2]
+				if ih > 960 or iw > 960:
+					img = cv2.resize(img, (960, 960))
+				cv2.imshow("img", img)
+				cv2.waitKey(0)
 
 		if cnt == 0:
 			continue
